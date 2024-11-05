de556af8f9e8e05a9efa112585d1de46a7ea4e5e
Mikael Rönnberg <mikael.rnnberg@gmail.com>
2024-11-05 10:24:12 +0100

    feat: added form button with loading spinner, and created topics list

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -10,6 +10,7 @@ import {
   PopoverContent,
 } from "@nextui-org/react";
 import * as actions from "@/actions";
+import FormButton from "@/components/common/form-button";
 
 export default function TopicCreateForm() {
   const [formState, action] = useActionState(actions.createTopic, {
@@ -48,8 +49,7 @@ export default function TopicCreateForm() {
                 {formState.errors._form?.join(" ")}
               </div>
             ) : null}
-
-            <Button type="submit">Submit</Button>
+            <FormButton>Save</FormButton>
           </div>
         </form>
       </PopoverContent>
